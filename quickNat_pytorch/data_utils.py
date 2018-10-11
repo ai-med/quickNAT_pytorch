cb14ad2c35cc90ebe7ff8a2bf59d7c0d6ec7199f
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-10-11 13:41:33 +0200

    Bug fixing in the data utils

--- quickNat_pytorch/data_utils.py
+++ quickNat_pytorch/data_utils.py
@@ -35,71 +35,25 @@ def get_data(data_params):
     Class_Weight_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_class_weights_file'] ), 'r')
     Weight_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_weights_file'] ), 'r')
     
-    return (ImdbData(Data_train['OASIS_data_train'], Label_train['OASIS_label_train'], Class_Weight_train['OASIS_class_weights_train']),
-            ImdbData(Data_test['OASIS_data_test'], Label_test['OASIS_label_test'], Class_Weight_test['OASIS_class_weights_train']))
+    return (ImdbData(Data_train['OASIS_data_train'][()], Label_train['OASIS_label_train'][()], Class_Weight_train['OASIS_class_weights_train'][()]),
+            ImdbData(Data_test['OASIS_data_test'][()], Label_test['OASIS_label_test'][()], Class_Weight_test['OASIS_class_weights_test'][()]))
     
-    
-def get_imdb_data():
-    # TODO: Need to change later
-    NumClass = 28
-    data_root = "../data/MALC_Coronal_Data/"
-    # Load DATA
-    Data = h5py.File(data_root+'Data.h5', 'r')
-    a_group_key = list(Data.keys())[0]
-    Data = list(Data[a_group_key])
-    Data = np.squeeze(np.asarray(Data))
-    Data = Data.astype(np.float32)
-    Label = h5py.File(data_root+'label.h5', 'r')
-    a_group_key = list(Label.keys())[0]
-    Label = list(Label[a_group_key])
-    Label = np.squeeze(np.asarray(Label))
-    Label = Label.astype(np.float32)
-    set = h5py.File(data_root+'set.h5', 'r')
-    a_group_key = list(set.keys())[0]
-    set = list(set[a_group_key])
-    set = np.squeeze(np.asarray(set))
-    sz = Data.shape
-    Data = Data.reshape([sz[0], 1, sz[1], sz[2]])
-    weights = Label[:,1,:,:]
-    Label = Label[:,0,:,:]
-    sz = Label.shape
-    print(sz)
-    Label = Label.reshape([sz[0], 1, sz[1], sz[2]])
-    weights = weights.reshape([sz[0], 1, sz[1], sz[2]])
-    train_id = set == 1
-    test_id = set == 3
 
-    Tr_Dat = Data[train_id, :, :, :]
-    Tr_Label = np.squeeze(Label[train_id, :, :, :]) - 1
-    Tr_weights = weights[train_id, :, :, :]
-    Tr_weights = np.tile(Tr_weights, [1, NumClass, 1, 1])
-    print(np.amax(Tr_Label))
-    print(np.amin(Tr_Label))
+ORIENTATION = {
+    'coronal': "COR",
+    'axial': "AXI",
+    'sagital': "SAG"
+}
 
-    Te_Dat = Data[test_id, :, :, :]
-    Te_Label = np.squeeze(Label[test_id, :, :, :]) - 1
-    Te_weights = weights[test_id, :, :, :]
-    Te_weights = np.tile(Te_weights, [1, NumClass, 1, 1])
-
-    del Data
-    del Label
-    del weights
-
-    # sz = Tr_Dat.shape
-    # sz_test = Te_Dat.shape
-    # y2 = np.ones((sz[0], NumClass, sz[2], sz[3]))
-    # y_test = np.ones((sz_test[0], NumClass, sz_test[2], sz_test[3]))
-    # for i in range(NumClass):
-    #     y2[:, i, :, :] = np.squeeze(np.multiply(np.ones(Tr_Label.shape), ((Tr_Label == i))))
-    #     y_test[:, i, :, :] = np.squeeze(np.multiply(np.ones(Te_Label.shape), ((Te_Label == i))))
-    #
-    # Tr_Label_bin = y2
-    # Te_Label_bin = y_test
-
-    return (ImdbData(Tr_Dat, Tr_Label,  Tr_weights),
-            ImdbData(Te_Dat, Te_Label,  Te_weights))
-    # return (ImdbData(Tr_Dat, Tr_Label, Tr_Label_bin, Tr_weights),
-    #         ImdbData(Te_Dat, Te_Label, Te_Label_bin, Te_weights))
+def _rotate_orientation(volume_data, volume_label, orientation=ORIENTATION['coronal']):
+    if orientation == ORIENTATION['coronal']:
+        return volume_data.transpose((2,0,1)), volume_label.transpose((2,0,1))
+    elif orientation == ORIENTATION['axial']:
+        return volume_data.transpose((1,2,0)), volume_label.transpose((1,2,0))        
+    elif orientation == ORIENTATION['sagital']:
+        return volume_data, volume_label
+    else:
+        raise ValueError("Invalid value for orientation. Pleas see help")
     
 def _estimate_weights_mfb(labels):
     class_weights = np.zeros_like(labels)
@@ -153,7 +107,7 @@ def _select_slices(data, labels, skip_Frame = 40):
 
 
     
-def _convertToHd5(data_dir, label_dir, volumes_txt_file , remap_config):
+def _convertToHd5(data_dir, label_dir, volumes_txt_file , remap_config, orientation=ORIENTATION['coronal']):
     """
     
     """
@@ -165,6 +119,7 @@ def _convertToHd5(data_dir, label_dir, volumes_txt_file , remap_config):
     
     for file_path in file_paths:
         volume_data, volume_label = nb.load(file_path[0]).get_fdata(), nb.load(file_path[1]).get_fdata()
+        volume_data, volume_label = _rotate_orientation(volume_data, volume_label, orientation)
         volume_data = (volume_data - np.min(volume_data)) / (np.max(volume_data) - np.min(volume_data))
         data, labels = _select_slices(volume_data, volume_label)
         labels = _remap_labels(labels, remap_config)
@@ -183,13 +138,13 @@ if __name__=="__main__":
     parser.add_argument('--label_dir', '-ld',required = True,  help = 'Base directory of all the label files. This folder should have one file per volumn with same name as the corresponding volumn folder name inside data_dir')
     parser.add_argument('--train_volumes', '-trv',required = True, help = 'Path to a text file containing the list of volumes to be used for training')
     parser.add_argument('--test_volumes', '-tev', required = True, help = 'Path to a text file containing the list of volumes to be used for testing')
-    parser.add_argument('--remap_config', '-rc', required = True, help = 'Valid options are "FS" and "Neo"')     
-    parser.add_argument('--destination_folder', '-df', help = 'Path where to generate the h5 files')    
-    
+    parser.add_argument('--remap_config', '-rc', required = True, help = 'Valid options are "FS" and "Neo"')
+    parser.add_argument('--orientation', '-o', required = True, help = 'Valid options are COR, AXI, SAG')
+    parser.add_argument('--destination_folder', '-df', help = 'Path where to generate the h5 files')
     
     args = parser.parse_args()
-    data_train, label_train, class_weights_train, weights_train = _convertToHd5(args.data_dir, args.label_dir, args.train_volumes, args.remap_config)
-    data_test, label_test, class_weights_test,  weights_test = _convertToHd5(args.data_dir, args.label_dir, args.test_volumes, args.remap_config)
+    data_train, label_train, class_weights_train, weights_train = _convertToHd5(args.data_dir, args.label_dir, args.train_volumes, args.remap_config, args.orientation)
+    data_test, label_test, class_weights_test,  weights_test = _convertToHd5(args.data_dir, args.label_dir, args.test_volumes, args.remap_config, args.orientation)
         
     DESTINATION_FOLDER = args.destination_folder if args.destination_folder else ""
     
@@ -211,4 +166,4 @@ if __name__=="__main__":
         label_test_handle.create_dataset("OASIS_label_test", data = label_test)
         class_weights_test_handle.create_dataset("OASIS_class_weights_test", data = class_weights_test)         
         weights_test_handle.create_dataset("OASIS_weights_test", data = weights_test)                
-        
+               
