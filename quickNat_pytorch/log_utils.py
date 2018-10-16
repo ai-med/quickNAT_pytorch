4664da1be16dbb01c010c081e6220c856b98d8bf
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-10-16 17:09:18 +0200

    Reformatiing

--- quickNat_pytorch/log_utils.py
+++ quickNat_pytorch/log_utils.py
@@ -7,6 +7,7 @@ from textwrap import wrap
 from sklearn.metrics import confusion_matrix
 import itertools
 import torchnet as tnt
+import torch
 
 plt.switch_backend('agg')
 plt.axis('scaled')
@@ -41,12 +42,23 @@ class LogWriter:
         predicted_labels = predicted_labels.view(-1, num_classes)
         correct_labels= correct_labels.view(-1)
         self._cm[phase].add(predicted_labels, correct_labels)
-#        cm = confusion_matrix(correct_labels.flatten(), predicted_labels.flatten(), labels=range(len(labels)))
-#        if normalize:
-#            cm = cm.astype('float')*10 / cm.sum(axis=1)[:, np.newaxis]
-#            cm = np.nan_to_num(cm, copy=True)
-#            cm = cm.astype('int')        
-#        self._cm[phase].append(cm)
+    
+    def image_per_epoch(self, prediction, groud_truth, phase, epoch):
+        max_val, idx = torch.max(prediction,1)
+        idx = idx.data.cpu().numpy()
+        idx = np.squeeze(idx)
+        fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(20,20))
+        ax[0].imshow(idx)
+        ax[0].set_title("Predicted", fontsize=10, color = "blue")
+        ax[0].axis('off')
+        ax[1].imshow(groud_truth)
+        ax[1].set_title("Ground Truth", fontsize=10, color = "blue")
+        ax[1].axis('off')
+        
+        if phase == 'train':
+            self.train_writer.add_figure('sample_prediction/' + phase, fig, epoch)
+        else:
+            self.val_writer.add_figure('sample_prediction/' + phase, fig, epoch)
             
     def reset_cms(self):
         for key, item in self._cm.items():
