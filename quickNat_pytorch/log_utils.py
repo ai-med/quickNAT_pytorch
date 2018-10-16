67969e24e774ab0db10488d8baf49ff032546f0f
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-10-16 12:11:59 +0200

    confusion matrix implementation

--- quickNat_pytorch/log_utils.py
+++ quickNat_pytorch/log_utils.py
@@ -10,17 +10,16 @@ import torchnet as tnt
 
 plt.switch_backend('agg')
 plt.axis('scaled')
-np.set_printoptions(precision=2)
-
 
 class LogWriter:
-    def __init__(self, num_classes= 33):
+    def __init__(self, num_classes= 33, cm_cmap = plt.cm.Blues, cm_normalized= True):
         self.train_writer = SummaryWriter("logs/train")
-        self.val_writer = SummaryWriter("logs/val") 
-        print(num_classes)
+        self.val_writer = SummaryWriter("logs/val")
+        self.cm_cmap = cm_cmap
+        self.cm_normalized = cm_normalized
         self._cm = {
-            'train': tnt.meter.ConfusionMeter(num_classes, normalized=True),
-            'val': tnt.meter.ConfusionMeter(num_classes, normalized=True)
+            'train': tnt.meter.ConfusionMeter(num_classes, normalized=cm_normalized),
+            'val': tnt.meter.ConfusionMeter(num_classes, normalized=cm_normalized)
         }
         self.fig = plt.figure() #For confusion matrix
 
@@ -50,19 +49,22 @@ class LogWriter:
 #        self._cm[phase].append(cm)
             
     def reset_cms(self):
-        self.cm = {key: [] for key, value in self.cm.items()}
+        for key, item in self._cm.items():
+            item.reset()
         
     def cm_per_epoch(self, labels, phase, epoch, iteration):
         cm = self._cm[phase].value()
        
-        fig = matplotlib.figure.Figure(figsize=(7, 7), dpi=180, facecolor='w', edgecolor='k')
+        fig = matplotlib.figure.Figure(figsize=(10, 10), dpi=360, facecolor='w', edgecolor='k')
         ax = fig.add_subplot(1, 1, 1)
+        
         classes = [re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', x) for x in labels]
         classes = ['\n'.join(wrap(l, 40)) for l in classes]
 
         tick_marks = np.arange(len(classes))
-
-        ax.set_xlabel('Predicted', fontsize=7)
+        
+        ax.imshow(cm, interpolation = 'nearest', cmap=self.cm_cmap)
+        ax.set_xlabel('Predicted', fontsize=7)        
         ax.set_xticks(tick_marks)
         c = ax.set_xticklabels(classes, fontsize=4, rotation=-90,  ha='center')
         ax.xaxis.set_label_position('bottom')
@@ -74,10 +76,13 @@ class LogWriter:
         ax.yaxis.set_label_position('left')
         ax.yaxis.tick_left()
 
+        fmt = '.2f' if self.cm_normalized else 'd'
+        thresh = cm.max() / 2.
         for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-            ax.text(j, i, format(cm[i, j], 'd') if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=6, verticalalignment='center', color= "black")
+            ax.text(j, i, format(cm[i, j], fmt) if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=6, verticalalignment='center', color= "white" if cm[i, j] > thresh else "black")
             
         fig.set_tight_layout(True)  
+        np.set_printoptions(precision=2)
         if phase == 'train':
             self.train_writer.add_figure('confusion_matrix/' + phase, fig, epoch)
         else:
