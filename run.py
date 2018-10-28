5b2ce7be025c21f7bdcf39c23927b7f2da19ebba
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-10-29 00:04:57 +0100

    Project restructuring

--- run.py
+++ run.py
@@ -1,54 +1,93 @@
+##TODO: Not a correct version. Need to work on it
+
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
-from torch.autograd import Variable
-from quickNat_pytorch.solver import Solver
-from quickNat_pytorch.quickNAT import quickNAT
-from quickNat_pytorch.data_utils import get_imdb_data
 
-#torch.set_default_tensor_type('torch.FloatTensor')
+from quicknat import QuickNat
+from utils.data_utils import get_data
+from utils.log_utils import LogWriter
+import utils.evaluator as eu
+from settings import Settings
+from solver import Solver
+import argparse
 
-plt.rcParams['figure.figsize'] = (10.0, 10.0) # set default size of plots
-plt.rcParams['image.interpolation'] = 'nearest'
-plt.rcParams['image.cmap'] = 'gray'
+torch.set_default_tensor_type('torch.FloatTensor')
 
-train_data, test_data = get_imdb_data()
-print("Train size: %i" % len(train_data))
-print("Test size: %i" % len(test_data))
+def load_data(data_params):
+    print("Loading dataset")
+    train_data, test_data = get_data(data_params)
+    print("Train size: %i" % len(train_data))
+    print("Test size: %i" % len(test_data))
+    return train_data, test_data
 
+def train(train_params, common_params, data_params, net_params):
+    train_data, test_data = load_data(data_params)    
+    
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=train_params['train_batch_size'], shuffle=True, num_workers=4, pin_memory = True)
+    val_loader = torch.utils.data.DataLoader(test_data, batch_size=train_params['val_batch_size'], shuffle=False, num_workers=4, pin_memory = True)
 
-train_loader = torch.utils.data.DataLoader(train_data, batch_size=4, shuffle=True, num_workers=4)
-val_loader = torch.utils.data.DataLoader(test_data, batch_size=4, shuffle=False, num_workers=4)
+    quicknat_model = QuickNat(net_params)
 
-param ={
-        'num_channels':1,
-        'num_filters':64,
-        'kernel_h':5,
-        'kernel_w':5,
-        'kernel_c': 1,
-        'stride_conv':1,
-        'pool':2,
-        'stride_pool':2,
-        'num_class':28
-    }
+    solver = Solver(quicknat_model,
+                    device = common_params['device'],
+                    num_class = net_params['num_class'],
+                    optim_args={"lr": train_params['learning_rate'],
+                                "betas": train_params['optim_betas'], 
+                                "eps": train_params['optim_eps'], 
+                                "weight_decay": train_params['optim_weight_decay']},
+                    model_name = common_params['model_name'],
+                    labels = data_params['labels'],
+                    log_nth=train_params['log_nth'], 
+                    num_epochs=train_params['num_epochs'], 
+                    log_dir_name = common_params['log_dir_name'],
+                    exp_dir_name=train_params['exp_dir_name'], 
+                    lr_scheduler_step_size = train_params['lr_scheduler_step_size'], 
+                    lr_scheduler_gamma = train_params['lr_scheduler_gamma'],
+                    use_last_checkpoint =train_params['use_last_checkpoint'],
+                    save_model_dir = common_params['save_model_dir'])
 
-exp_dir_name = 'Exp01_try'
+    solver.train(train_loader, val_loader)
+    
+    quicknat_model.save(train_params['final_model_path'])
+    
+def evaluate(eval_params, net_params, data_params, common_params, train_params):
+    eval_model_path = eval_params['eval_model_path']
+    num_classes = net_params['num_class']
+    labels = data_params['labels']
+    data_dir = eval_params['data_dir']
+    label_dir = eval_params['label_dir']
+    volumes_txt_file = eval_params['volumes_txt_file']
+    remap_config = eval_params['remap_config']
+    device = common_params['device']
+    log_dir_name = common_params['log_dir_name']
+    exp_dir_name = train_params['exp_dir_name']
+    orientation = eval_params['orientation']
 
-quicknat_model = quickNAT(param)
-solver = Solver(optim_args={"lr": 1e-2})
-solver.train(quicknat_model, train_loader, val_loader, log_nth=10, num_epochs=10, exp_dir_name=exp_dir_name)
+    logWriter = LogWriter(num_classes, log_dir_name, exp_dir_name, labels=labels)
 
-
-quicknat_model.save("models/quicknat_model.model")
-
-out = quicknat_model(Variable(torch.Tensor(test_data.X[11:12]).cuda(),volatile=True))
-max_val, idx = torch.max(out,1)
-idx = idx.data.cpu().numpy()
-idx = np.squeeze(idx==2)
-plt.imshow(idx)
-plt.show()
-
-img_test = test_data.X[11:12]
-img_test = np.squeeze(img_test)
-plt.imshow(img_test)
-plt.show()
+    avg_dice_score, volume_dice_score_list = eu.evaluate_dice_score(eval_model_path, 
+                                                                    num_classes, 
+                                                                    data_dir, 
+                                                                    label_dir, 
+                                                                    volumes_txt_file , 
+                                                                    remap_config, 
+                                                                    orientation, 
+                                                                    device, 
+                                                                    logWriter)    
+    
+if __name__ == '__main__':
+    
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--mode','-m', required = True, help = 'run mode, valid values are train and eval')    
+    args = parser.parse_args()
+    
+    settings = Settings()
+    common_params, data_params, net_params, train_params, eval_params = settings['COMMON'], settings['DATA'], settings['NETWORK'], settings['TRAINING'], settings['EVAL']
+    
+    if args.mode == 'train':
+        train(train_params, common_params, data_params, net_params)
+    elif args.mode == 'eval':
+        evaluate(eval_params, net_params, data_params, common_params, train_params)
+    else:
+        raise ValueError('Invalid value for mode. only support values are train and eval')
---
+++ utils/log_utils.py
@@ -0,0 +1,157 @@
+from tensorboardX import SummaryWriter
+import matplotlib.pyplot as plt
+import matplotlib
+import numpy as np
+import re
+from textwrap import wrap
+import itertools
+import torch
+import math
+import pandas as pd
+import os
+import shutil
+import utils.evaluator as eu
+
+plt.switch_backend('agg')
+plt.axis('scaled')
+
+class LogWriter(object):
+    def __init__(self, num_class, log_dir_name, exp_dir_name, use_last_checkpoint=False, labels=None, cm_cmap = plt.cm.Blues):
+        self.num_class=num_class
+        train_log_path, val_log_path = os.path.join(log_dir_name, exp_dir_name, "train"), os.path.join(log_dir_name, exp_dir_name, "val")
+        if not use_last_checkpoint:
+            if os.path.exists(train_log_path):
+                shutil.rmtree(train_log_path)
+            if os.path.exists(val_log_path):
+                shutil.rmtree(val_log_path)
+                
+        self.writer = {
+            'train' : SummaryWriter(train_log_path),
+            'val' : SummaryWriter(val_log_path)
+        }
+        self.curr_iter = 1
+        self.cm_cmap = cm_cmap
+        self.init_cm()
+        self.init_ds()
+        self.labels = self.beautify_labels(labels)
+        
+    def loss_per_iter(self, loss_value, i_batch, current_iteration):
+        print('[Iteration : ' + str(i_batch) + '] Loss -> ' + str(loss_value))
+        self.writer['train'].add_scalar('loss/per_iteration', loss_value, current_iteration)
+        
+    def loss_per_epoch(self, loss_arr, phase, epoch):
+        writer = self.writer[phase]
+        if phase == 'train':
+            loss = loss_arr[-1]
+        else:
+            loss = np.mean(loss_arr)
+        self.writer[phase].add_scalar('loss/per_epoch', loss, epoch)            
+        print('epoch '+phase + ' loss = ' + str(loss))
+       
+    def update_cm_per_iter(self, predictions, correct_labels, phase): 
+        _, batch_output = torch.max(predictions, dim=1)
+        _, cm_batch = eu.dice_confusion_matrix(batch_output, correct_labels, self.num_class)
+        self._cm[phase]+=cm_batch.cpu()
+        del cm_batch, batch_output       
+            
+    def cm_per_epoch(self, phase, epoch, i_batch):
+        print("Confusion Matrix...", end='')
+        cm = (self._cm[phase] / (i_batch + 1)).cpu().numpy()
+        self.plot_cm('confusion_matrix', phase, cm, epoch)
+        self.init_cm()
+        print("DONE")
+        
+        
+    def plot_cm(self, caption, phase, cm, step=None):
+        fig = matplotlib.figure.Figure(figsize=(8, 8), dpi=180, facecolor='w', edgecolor='k')
+        ax = fig.add_subplot(1, 1, 1)
+                
+        ax.imshow(cm, interpolation = 'nearest', cmap=self.cm_cmap)
+        ax.set_xlabel('Predicted', fontsize=7)        
+        ax.set_xticks(np.arange(self.num_class))
+        c = ax.set_xticklabels(self.labels, fontsize=4, rotation=-90,  ha='center')
+        ax.xaxis.set_label_position('bottom')
+        ax.xaxis.tick_bottom()
+
+        ax.set_ylabel('True Label', fontsize=7)
+        ax.set_yticks(np.arange(self.num_class))
+        ax.set_yticklabels(self.labels, fontsize=4, va ='center')
+        ax.yaxis.set_label_position('left')
+        ax.yaxis.tick_left()
+
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            ax.text(j, i, format(cm[i, j], '.2f') if cm[i,j]!=0 else '.', horizontalalignment="center", fontsize=6, verticalalignment='center', color= "white" if cm[i, j] > thresh else "black")
+            
+        fig.set_tight_layout(True)  
+        np.set_printoptions(precision=2)
+        if step:
+            self.writer[phase].add_figure(caption + '/' + phase, fig, step)
+        else:
+            self.writer[phase].add_figure(caption + '/' + phase, fig)
+            
+        
+    def update_dice_score_per_iteration(self, predictions, correct_labels, epoch):
+        _, batch_output = torch.max(predictions, dim=1)
+        score_vector = eu.dice_score_perclass(batch_output, correct_labels, self.num_class)
+        self._ds +=  score_vector.cpu()
+        
+    def dice_score_per_epoch(self, epoch, i_batch):
+        print("Dice Score...", end='')
+        ds = (self._ds / (i_batch + 1)).cpu().numpy()
+        self.plot_dice_score('dice_score_per_epoch', ds, 'Dice Score', epoch)      
+        self.init_ds() 
+        print("DONE")
+        
+    def plot_dice_score(self, caption, ds, title, step=None):
+        fig = matplotlib.figure.Figure(figsize=(7, 4), dpi=180, facecolor='w', edgecolor='k')
+        ax = fig.add_subplot(1, 1, 1)            
+        ax.set_xlabel(title, fontsize=10)  
+        ax.xaxis.set_label_position('top')            
+        ax.bar(np.arange(self.num_class), ds)
+        ax.set_xticks(np.arange(self.num_class))
+        c = ax.set_xticklabels(self.labels, fontsize=8, rotation=-90,  ha='center')
+        ax.xaxis.tick_bottom()
+        if step:
+            self.writer['val'].add_figure(caption, fig, step)
+        else:
+            self.writer['val'].add_figure(caption, fig)
+
+    def image_per_epoch(self, prediction, ground_truth, phase, epoch):
+        print("Sample Images...", end='')
+        ncols = 2
+        nrows = len(prediction)
+        fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(10, 20))
+        
+        for i in range(nrows):
+            ax[i][0].imshow(prediction[i], cmap = 'CMRmap', vmin=0, vmax=self.num_class-1)
+            ax[i][0].set_title("Predicted", fontsize=10, color = "blue")
+            ax[i][0].axis('off')
+            ax[i][1].imshow(ground_truth[i], cmap = 'CMRmap', vmin=0, vmax=self.num_class-1)
+            ax[i][1].set_title("Ground Truth", fontsize=10, color = "blue")
+            ax[i][1].axis('off')
+        fig.set_tight_layout(True)  
+        self.writer[phase].add_figure('sample_prediction/' + phase, fig, epoch) 
+        print('DONE')
+        
+    def graph(self, model, X):
+        self.writer['train'].add_graph(model, X) 
+        
+    def close(self):
+        self.writer['train'].close()
+        self.writer['val'].close()
+        
+    def init_cm(self):
+        self._cm = {
+            'train': torch.zeros(self.num_class, self.num_class),
+            'val': torch.zeros(self.num_class, self.num_class)
+        }
+        
+    def init_ds(self):
+        self._ds = torch.zeros(self.num_class)
+        
+    def beautify_labels(self, labels):
+        classes = [re.sub(r'([a-z](?=[A-Z])|[A-Z](?=[A-Z][a-z]))', r'\1 ', x) for x in labels]
+        classes = ['\n'.join(wrap(l, 40)) for l in classes]
+        return classes        
+        
