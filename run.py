448a8717d28f00beee93aded8c6aee36eca348a9
Shayan Ahmad Siddiqui <shayan.siddiqui89@gmail.com>
2018-10-30 00:28:49 +0100

    code cleanup

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 ##TODO: Not a correct version. Need to work on it
 
-import numpy as np
-import matplotlib.pyplot as plt
-import torch
-
-from quicknat import QuickNat
-from utils.data_utils import get_data
-from utils.log_utils import LogWriter
-import utils.evaluator as eu
-from settings import Settings
-from solver import Solver
 import argparse
 
+import torch
+
+import utils.evaluator as eu
+from quicknat import QuickNat
+from settings import Settings
+from solver import Solver
+from utils.data_utils import get_data
+from utils.log_utils import LogWriter
+
 torch.set_default_tensor_type('torch.FloatTensor')
+
 
 def load_data(data_params):
     print("Loading dataset")
@@ -21,36 +21,40 @@ def load_data(data_params):
     print("Test size: %i" % len(test_data))
     return train_data, test_data
 
+
 def train(train_params, common_params, data_params, net_params):
-    train_data, test_data = load_data(data_params)    
-    
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=train_params['train_batch_size'], shuffle=True, num_workers=4, pin_memory = True)
-    val_loader = torch.utils.data.DataLoader(test_data, batch_size=train_params['val_batch_size'], shuffle=False, num_workers=4, pin_memory = True)
+    train_data, test_data = load_data(data_params)
+
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=train_params['train_batch_size'], shuffle=True,
+                                               num_workers=4, pin_memory=True)
+    val_loader = torch.utils.data.DataLoader(test_data, batch_size=train_params['val_batch_size'], shuffle=False,
+                                             num_workers=4, pin_memory=True)
 
     quicknat_model = QuickNat(net_params)
 
     solver = Solver(quicknat_model,
-                    device = common_params['device'],
-                    num_class = net_params['num_class'],
+                    device=common_params['device'],
+                    num_class=net_params['num_class'],
                     optim_args={"lr": train_params['learning_rate'],
-                                "betas": train_params['optim_betas'], 
-                                "eps": train_params['optim_eps'], 
+                                "betas": train_params['optim_betas'],
+                                "eps": train_params['optim_eps'],
                                 "weight_decay": train_params['optim_weight_decay']},
-                    model_name = common_params['model_name'],
-                    labels = data_params['labels'],
-                    log_nth=train_params['log_nth'], 
-                    num_epochs=train_params['num_epochs'], 
-                    log_dir_name = common_params['log_dir_name'],
-                    exp_dir_name=train_params['exp_dir_name'], 
-                    lr_scheduler_step_size = train_params['lr_scheduler_step_size'], 
-                    lr_scheduler_gamma = train_params['lr_scheduler_gamma'],
-                    use_last_checkpoint =train_params['use_last_checkpoint'],
-                    save_model_dir = common_params['save_model_dir'])
+                    model_name=common_params['model_name'],
+                    labels=data_params['labels'],
+                    log_nth=train_params['log_nth'],
+                    num_epochs=train_params['num_epochs'],
+                    log_dir_name=common_params['log_dir_name'],
+                    exp_dir_name=train_params['exp_dir_name'],
+                    lr_scheduler_step_size=train_params['lr_scheduler_step_size'],
+                    lr_scheduler_gamma=train_params['lr_scheduler_gamma'],
+                    use_last_checkpoint=train_params['use_last_checkpoint'],
+                    save_model_dir=common_params['save_model_dir'])
 
     solver.train(train_loader, val_loader)
-    
+
     quicknat_model.save(train_params['final_model_path'])
-    
+
+
 def evaluate(eval_params, net_params, data_params, common_params, train_params):
     eval_model_path = eval_params['eval_model_path']
     num_classes = net_params['num_class']
@@ -66,25 +70,27 @@ def evaluate(eval_params, net_params, data_params, common_params, train_params):
 
     logWriter = LogWriter(num_classes, log_dir_name, exp_dir_name, labels=labels)
 
-    avg_dice_score, volume_dice_score_list = eu.evaluate_dice_score(eval_model_path, 
-                                                                    num_classes, 
-                                                                    data_dir, 
-                                                                    label_dir, 
-                                                                    volumes_txt_file , 
-                                                                    remap_config, 
-                                                                    orientation, 
-                                                                    device, 
-                                                                    logWriter)    
-    
+    avg_dice_score, volume_dice_score_list = eu.evaluate_dice_score(eval_model_path,
+                                                                    num_classes,
+                                                                    data_dir,
+                                                                    label_dir,
+                                                                    volumes_txt_file,
+                                                                    remap_config,
+                                                                    orientation,
+                                                                    device,
+                                                                    logWriter)
+
+
 if __name__ == '__main__':
-    
-    parser=argparse.ArgumentParser()
-    parser.add_argument('--mode','-m', required = True, help = 'run mode, valid values are train and eval')    
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', '-m', required=True, help='run mode, valid values are train and eval')
     args = parser.parse_args()
-    
+
     settings = Settings()
-    common_params, data_params, net_params, train_params, eval_params = settings['COMMON'], settings['DATA'], settings['NETWORK'], settings['TRAINING'], settings['EVAL']
-    
+    common_params, data_params, net_params, train_params, eval_params = settings['COMMON'], settings['DATA'], settings[
+        'NETWORK'], settings['TRAINING'], settings['EVAL']
+
     if args.mode == 'train':
         train(train_params, common_params, data_params, net_params)
     elif args.mode == 'eval':
--- utils/data_utils.py
+++ utils/data_utils.py
@@ -3,21 +3,23 @@ Data utility functions.
 Sample command to create new dataset - python quickNat_pytorch/data_utils.py -dd /home/masterthesis/shayan/nas_drive/Data_Neuro/OASISchallenge/FS -ld /home/masterthesis/shayan/nas_drive/Data_Neuro/OASISchallenge -trv datasets/train_volumes.txt -tev datasets/test_volumes.txt -rc Neo -o COR -df datasets/coronal
 """
 
+import argparse
 import os
+
+import h5py
+import nibabel as nb
 import numpy as np
 import torch
 import torch.utils.data as data
-import h5py
-import argparse
-import os
-import nibabel as nb
-import utils.preprocessor as preprocessor
+
+import preprocessor
+
 
 class ImdbData(data.Dataset):
     def __init__(self, X, y, w):
-        self.X = X if len(X.shape) == 4 else X[:,np.newaxis,:,:]
-        self.y = y 
-        self.w = w 
+        self.X = X if len(X.shape) == 4 else X[:, np.newaxis, :, :]
+        self.y = y
+        self.w = w
 
     def __getitem__(self, index):
         img = torch.from_numpy(self.X[index])
@@ -27,106 +29,118 @@ class ImdbData(data.Dataset):
 
     def __len__(self):
         return len(self.y)
-        
-def get_data(data_params):
-    Data_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_data_file'] ), 'r')
-    Label_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_label_file'] ), 'r')
-    Class_Weight_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_class_weights_file'] ), 'r')
-    Weight_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_weights_file'] ), 'r')
 
-    Data_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_data_file'] ), 'r')
-    Label_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_label_file'] ), 'r')
-    Class_Weight_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_class_weights_file'] ), 'r')
-    Weight_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_weights_file'] ), 'r')
-    
-    return (ImdbData(Data_train['OASIS_data_train'][()], Label_train['OASIS_label_train'][()], Class_Weight_train['OASIS_class_weights_train'][()]),
-            ImdbData(Data_test['OASIS_data_test'][()], Label_test['OASIS_label_test'][()], Class_Weight_test['OASIS_class_weights_test'][()]))
-    
-            
-#TODO: Need to defing a dynamic pipeline
-#TODO: Presets for training, prediction and evaluation
+
+def get_data(data_params):
+    Data_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_data_file']), 'r')
+    Label_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_label_file']), 'r')
+    Class_Weight_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_class_weights_file']), 'r')
+    Weight_train = h5py.File(os.path.join(data_params['base_dir'], data_params['train_weights_file']), 'r')
+
+    Data_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_data_file']), 'r')
+    Label_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_label_file']), 'r')
+    Class_Weight_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_class_weights_file']), 'r')
+    Weight_test = h5py.File(os.path.join(data_params['base_dir'], data_params['test_weights_file']), 'r')
+
+    return (ImdbData(Data_train['OASIS_data_train'][()], Label_train['OASIS_label_train'][()],
+                     Class_Weight_train['OASIS_class_weights_train'][()]),
+            ImdbData(Data_test['OASIS_data_test'][()], Label_test['OASIS_label_test'][()],
+                     Class_Weight_test['OASIS_class_weights_test'][()]))
+
+
+# TODO: Need to defing a dynamic pipeline
+# TODO: Presets for training, prediction and evaluation
 def load_and_preprocess(data_dir,
-                         label_dir,
-                         volumes_txt_file,
-                         orientation,
-                         return_weights = False,
-                         reduce_slices = False,
-                         remove_black = False,
-                         remap_config = None):
-    
-    
+                        label_dir,
+                        volumes_txt_file,
+                        orientation,
+                        return_weights=False,
+                        reduce_slices=False,
+                        remove_black=False,
+                        remap_config=None):
     with open(volumes_txt_file) as file_handle:
         volumes_to_use = file_handle.read().splitlines()
-    file_paths = [[os.path.join(data_dir, vol, 'mri/orig.mgz'), os.path.join(label_dir, vol+'_glm.mgz')] for vol in volumes_to_use]
-    
-    volume_list, labelmap_lits, class_weights_list, weights_list = [], [], [], []
-    
+    file_paths = [[os.path.join(data_dir, vol, 'mri/orig.mgz'), os.path.join(label_dir, vol + '_glm.mgz')] for vol in
+                  volumes_to_use]
+
+    volume_list, labelmap_list, class_weights_list, weights_list = [], [], [], []
+
     for file_path in file_paths:
         volume, labelmap = nb.load(file_path[0]).get_fdata(), nb.load(file_path[1]).get_fdata()
         volume = (volume - np.min(volume)) / (np.max(volume) - np.min(volume))
-        volume, labelmap = preprocessor.rotate_orientation(volume, labelmap, orientation)        
-        
+        volume, labelmap = preprocessor.rotate_orientation(volume, labelmap, orientation)
+
         if reduce_slices:
             volume, labelmap = preprocessor.reduce_slices(volume, labelmap)
-        
+
         if remap_config:
             labelmap = preprocessor.remap_labels(labelmap, remap_config)
-        
+
         if remove_black:
-            volume, labelmap = preprocessor.remove_black(volume, labelmap, 0)
-            
+            volume, labelmap = preprocessor.remove_black(volume, labelmap)
+
         volume_list.append(volume)
-        labelmap_lits.append(labelmap)
-        
+        labelmap_list.append(labelmap)
+
         if return_weights:
-            class_weights, weights = preprocessor.estimate_weights_mfb(labels)
+            class_weights, weights = preprocessor.estimate_weights_mfb(labelmap)
             class_weights_list.append(class_weights)
             weights_list.append(weights)
-        
+
     if return_weights:
-        return volume_list, labelmap_lits, class_weights_list, weights_list
+        return volume_list, labelmap_list, class_weights_list, weights_list
     else:
-        return volume_list, labelmap_lits
-    
-def _convertToHd5(data_dir, 
-                  label_dir, 
-                  volumes_txt_file, 
-                  remap_config, 
+        return volume_list, labelmap_list
+
+
+def _convertToHd5(data_dir,
+                  label_dir,
+                  volumes_txt_file,
+                  remap_config,
                   orientation=preprocessor.ORIENTATION['coronal']):
     """
     
     """
-    data_h5, label_h5, class_weights_h5, weights_h5 = load_and_preprocess(data_dir,label_dir,
+    data_h5, label_h5, class_weights_h5, weights_h5 = load_and_preprocess(data_dir, label_dir,
                                                                           volumes_txt_file,
                                                                           orientation,
-                                                                          return_weights = True,
-                                                                          reduce_slices = True,
-                                                                          remove_black = True,
-                                                                          remap_config = remap_config)
-        
-    no_slices, H, W = data_h5[0].shape
-    return np.concatenate(data_h5).reshape((-1, H, W)), np.concatenate(label_h5).reshape((-1, H, W)), np.concatenate(class_weights_h5).reshape((-1, H, W)), np.concatenate(weights_h5)
-    
+                                                                          return_weights=True,
+                                                                          reduce_slices=True,
+                                                                          remove_black=True,
+                                                                          remap_config=remap_config)
 
-if __name__=="__main__":
-    parser=argparse.ArgumentParser()
-    parser.add_argument('--data_dir','-dd', required = True, help = 'Base directory of the data folder. This folder should contain one folder per volume, and each volumn folder should have an orig.mgz file')
-    parser.add_argument('--label_dir', '-ld',required = True,  help = 'Base directory of all the label files. This folder should have one file per volumn with same name as the corresponding volumn folder name inside data_dir')
-    parser.add_argument('--train_volumes', '-trv',required = True, help = 'Path to a text file containing the list of volumes to be used for training')
-    parser.add_argument('--test_volumes', '-tev', required = True, help = 'Path to a text file containing the list of volumes to be used for testing')
-    parser.add_argument('--remap_config', '-rc', required = True, help = 'Valid options are "FS" and "Neo"')
-    parser.add_argument('--orientation', '-o', required = True, help = 'Valid options are COR, AXI, SAG')
-    parser.add_argument('--destination_folder', '-df', help = 'Path where to generate the h5 files')
-    
+    no_slices, H, W = data_h5[0].shape
+    return np.concatenate(data_h5).reshape((-1, H, W)), np.concatenate(label_h5).reshape((-1, H, W)), np.concatenate(
+        class_weights_h5).reshape((-1, H, W)), np.concatenate(weights_h5)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--data_dir', '-dd', required=True,
+                        help='Base directory of the data folder. This folder should contain one folder per volume, and each volumn folder should have an orig.mgz file')
+    parser.add_argument('--label_dir', '-ld', required=True,
+                        help='Base directory of all the label files. This folder should have one file per volumn with same name as the corresponding volumn folder name inside data_dir')
+    parser.add_argument('--train_volumes', '-trv', required=True,
+                        help='Path to a text file containing the list of volumes to be used for training')
+    parser.add_argument('--test_volumes', '-tev', required=True,
+                        help='Path to a text file containing the list of volumes to be used for testing')
+    parser.add_argument('--remap_config', '-rc', required=True, help='Valid options are "FS" and "Neo"')
+    parser.add_argument('--orientation', '-o', required=True, help='Valid options are COR, AXI, SAG')
+    parser.add_argument('--destination_folder', '-df', help='Path where to generate the h5 files')
+
     args = parser.parse_args()
-    data_train, label_train, class_weights_train, weights_train = _convertToHd5(args.data_dir, args.label_dir, args.train_volumes, args.remap_config, args.orientation)
-    data_test, label_test, class_weights_test,  weights_test = _convertToHd5(args.data_dir, args.label_dir, args.test_volumes, args.remap_config, args.orientation)
-        
+    data_train, label_train, class_weights_train, weights_train = _convertToHd5(args.data_dir, args.label_dir,
+                                                                                args.train_volumes, args.remap_config,
+                                                                                args.orientation)
+    data_test, label_test, class_weights_test, weights_test = _convertToHd5(args.data_dir, args.label_dir,
+                                                                            args.test_volumes, args.remap_config,
+                                                                            args.orientation)
+
     if args.destination_folder and not os.path.exists(args.destination_folder):
         os.makedirs(args.destination_folder)
-        
+
     DESTINATION_FOLDER = args.destination_folder if args.destination_folder else ""
-    
+
     DATA_TRAIN_FILE = os.path.join(DESTINATION_FOLDER, "Data_train.h5")
     LABEL_TRAIN_FILE = os.path.join(DESTINATION_FOLDER, "Label_train.h5")
     WEIGHTS_TRAIN_FILE = os.path.join(DESTINATION_FOLDER, "Weight_train.h5")
@@ -135,13 +149,19 @@ if __name__=="__main__":
     LABEL_TEST_FILE = os.path.join(DESTINATION_FOLDER, "Label_test.h5")
     WEIGHTS_TEST_FILE = os.path.join(DESTINATION_FOLDER, "Weight_test.h5")
     CLASS_WEIGHTS_TEST_FILE = os.path.join(DESTINATION_FOLDER, "Class_Weight_test.h5")
-    
-    with h5py.File(DATA_TRAIN_FILE,"w") as data_train_handle, h5py.File(LABEL_TRAIN_FILE,"w") as label_train_handle, h5py.File(WEIGHTS_TRAIN_FILE,"w") as weights_train_handle,h5py.File(CLASS_WEIGHTS_TRAIN_FILE,"w") as class_weights_train_handle, h5py.File(DATA_TEST_FILE,"w") as data_test_handle, h5py.File(LABEL_TEST_FILE,"w") as label_test_handle, h5py.File(WEIGHTS_TEST_FILE,"w") as weights_test_handle, h5py.File(CLASS_WEIGHTS_TEST_FILE,"w") as class_weights_test_handle:
-        data_train_handle.create_dataset("OASIS_data_train", data = data_train)
-        label_train_handle.create_dataset("OASIS_label_train", data = label_train)
-        class_weights_train_handle.create_dataset("OASIS_class_weights_train", data = class_weights_train)
-        weights_train_handle.create_dataset("OASIS_weights_train", data = weights_train)
-        data_test_handle.create_dataset("OASIS_data_test", data = data_test)
-        label_test_handle.create_dataset("OASIS_label_test", data = label_test)
-        class_weights_test_handle.create_dataset("OASIS_class_weights_test", data = class_weights_test)
-        weights_test_handle.create_dataset("OASIS_weights_test", data = weights_test)
+
+    with h5py.File(DATA_TRAIN_FILE, "w") as data_train_handle, h5py.File(LABEL_TRAIN_FILE,
+                                                                         "w") as label_train_handle, h5py.File(
+        WEIGHTS_TRAIN_FILE, "w") as weights_train_handle, h5py.File(CLASS_WEIGHTS_TRAIN_FILE,
+                                                                    "w") as class_weights_train_handle, h5py.File(
+        DATA_TEST_FILE, "w") as data_test_handle, h5py.File(LABEL_TEST_FILE, "w") as label_test_handle, h5py.File(
+        WEIGHTS_TEST_FILE, "w") as weights_test_handle, h5py.File(CLASS_WEIGHTS_TEST_FILE,
+                                                                  "w") as class_weights_test_handle:
+        data_train_handle.create_dataset("OASIS_data_train", data=data_train)
+        label_train_handle.create_dataset("OASIS_label_train", data=label_train)
+        class_weights_train_handle.create_dataset("OASIS_class_weights_train", data=class_weights_train)
+        weights_train_handle.create_dataset("OASIS_weights_train", data=weights_train)
+        data_test_handle.create_dataset("OASIS_data_test", data=data_test)
+        label_test_handle.create_dataset("OASIS_label_test", data=label_test)
+        class_weights_test_handle.create_dataset("OASIS_class_weights_test", data=class_weights_test)
+        weights_test_handle.create_dataset("OASIS_weights_test", data=weights_test)
